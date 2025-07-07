c8271f12b13fdc88091b192ca203682e502358ee
Tucom868 <marvin.peters2017@gmail.com>
2025-07-07 13:32:53 -0400

    Add Bot Script

---
+++ telegram_alerts.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    try:
+        requests.post(url, data=payload)
+    except Exception as e:
+        print(f"Telegram Error: {e}")
